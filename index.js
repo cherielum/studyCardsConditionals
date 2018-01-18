6fbc430b844c54e795978815bab5fff706a66d2d
Cherie Lum <charismatic.cherie@gmail.com>
2018-01-18 18:58:11 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,40 @@
+$('.container').on('click', function(){
+    console.log(123);
+    $('.card').toggleClass('flipped');
+});
+
+//first card
+$('.container1').on('click', function(){
+    console.log(123);
+    $('.card1').toggleClass('flipped');
+});
+
+//second card
+$('.container2').on('click', function(){
+    console.log(123);
+    $('.card2').toggleClass('flipped');
+});
+
+//third card
+$('.container3').on('click', function(){
+    console.log(123);
+    $('.card3').toggleClass('flipped');
+});
+
+//fourth card
+$('.container4').on('click', function(){
+    console.log(123);
+    $('.card4').toggleClass('flipped');
+});
+
+//fifth card
+$('.container5').on('click', function(){
+    console.log(123);
+    $('.card5').toggleClass('flipped');
+});
+
+//sixth card
+$('.container6').on('click', function(){
+    console.log(123);
+    $('.card6').toggleClass('flipped');
+});
